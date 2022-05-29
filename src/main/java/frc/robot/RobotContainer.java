0082169532b15ecf5cb148b1f4833407b78b67ec
Daniel Quednow <dqued56@tscstudents.net>
2022-05-29 15:29:08 -0400

    Change settings, reformat, and remove an import
    Changed format settings (4 spaces per indent), reformatted all the
    files that needed it, and removed an unnecesary import from Utils.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,82 +19,88 @@ import static frc.robot.utils.Utils.*;
 import static frc.robot.Constants.*;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's controllers are defined here..
-  private XboxController m_driveController = new XboxController(0);
+    // The robot's controllers are defined here..
+    private XboxController m_driveController = new XboxController(0);
 
-  private JoystickButton m_driveAButton = joystickButton(m_driveController, kA);
-  private JoystickButton m_driveBButton = joystickButton(m_driveController, kB);
-  private JoystickButton m_driveXButton = joystickButton(m_driveController, kX);
-  private JoystickButton m_driveYButton = joystickButton(m_driveController, kY);
+    private JoystickButton m_driveAButton = joystickButton(m_driveController, kA);
+    private JoystickButton m_driveBButton = joystickButton(m_driveController, kB);
+    private JoystickButton m_driveXButton = joystickButton(m_driveController, kX);
+    private JoystickButton m_driveYButton = joystickButton(m_driveController, kY);
 
-  private JoystickButton m_driveStartButton = joystickButton(m_driveController, kStart);
-  private JoystickButton m_driveBackButton = joystickButton(m_driveController, kBack);
+    private JoystickButton m_driveStartButton = joystickButton(m_driveController, kStart);
+    private JoystickButton m_driveBackButton = joystickButton(m_driveController, kBack);
 
-  private JoystickButton m_driveLeftBumper = joystickButton(m_driveController, kLeftBumper);
-  private JoystickButton m_driveRightBumper = joystickButton(m_driveController, kRightBumper);
+    private JoystickButton m_driveLeftBumper = joystickButton(m_driveController, kLeftBumper);
+    private JoystickButton m_driveRightBumper = joystickButton(m_driveController, kRightBumper);
 
-  private JoystickButton m_driveLeftStick = joystickButton(m_driveController, kLeftStick);
-  private JoystickButton m_driveRightStick = joystickButton(m_driveController, kRightBumper);
+    private JoystickButton m_driveLeftStick = joystickButton(m_driveController, kLeftStick);
+    private JoystickButton m_driveRightStick = joystickButton(m_driveController, kRightBumper);
 
-  private JoystickTrigger m_driveLeftTrigger = joystickTrigger(m_driveController, kLeftTrigger);
-  private JoystickTrigger m_driveRightTrigger = joystickTrigger(m_driveController, kRightTrigger);
+    private JoystickTrigger m_driveLeftTrigger = joystickTrigger(m_driveController, kLeftTrigger);
+    private JoystickTrigger m_driveRightTrigger = joystickTrigger(m_driveController, kRightTrigger);
 
-  private JoystickAxes m_driveLeftJoystick = joystickAxes(m_driveController, kLeftX, kLeftY);
-  private JoystickAxes m_driveRightJoystick = joystickAxes(m_driveController, kRightX, kRightY);
+    private JoystickAxes m_driveLeftJoystick = joystickAxes(m_driveController, kLeftX, kLeftY);
+    private JoystickAxes m_driveRightJoystick = joystickAxes(m_driveController, kRightX, kRightY);
 
+    private XboxController m_operatorController = new XboxController(1);
 
-  private XboxController m_operatorController = new XboxController(1);
+    private JoystickButton m_operatorAButton = joystickButton(m_operatorController, kA);
+    private JoystickButton m_operatorBButton = joystickButton(m_operatorController, kB);
+    private JoystickButton m_operatorXButton = joystickButton(m_operatorController, kX);
+    private JoystickButton m_operatorYButton = joystickButton(m_operatorController, kY);
 
-  private JoystickButton m_operatorAButton = joystickButton(m_operatorController, kA);
-  private JoystickButton m_operatorBButton = joystickButton(m_operatorController, kB);
-  private JoystickButton m_operatorXButton = joystickButton(m_operatorController, kX);
-  private JoystickButton m_operatorYButton = joystickButton(m_operatorController, kY);
+    private JoystickButton m_operatorStartButton = joystickButton(m_operatorController, kStart);
+    private JoystickButton m_operatorBackButton = joystickButton(m_operatorController, kBack);
 
-  private JoystickButton m_operatorStartButton = joystickButton(m_operatorController, kStart);
-  private JoystickButton m_operatorBackButton = joystickButton(m_operatorController, kBack);
+    private JoystickButton m_operatorLeftBumper = joystickButton(m_operatorController, kLeftBumper);
+    private JoystickButton m_operatorRightBumper = joystickButton(m_operatorController, kRightBumper);
 
-  private JoystickButton m_operatorLeftBumper = joystickButton(m_operatorController, kLeftBumper);
-  private JoystickButton m_operatorRightBumper = joystickButton(m_operatorController, kRightBumper);
+    private JoystickButton m_operatorLeftStick = joystickButton(m_operatorController, kLeftStick);
+    private JoystickButton m_operatorRightStick = joystickButton(m_operatorController, kRightBumper);
 
-  private JoystickButton m_operatorLeftStick = joystickButton(m_operatorController, kLeftStick);
-  private JoystickButton m_operatorRightStick = joystickButton(m_operatorController, kRightBumper);
+    private JoystickTrigger m_operatorLeftTrigger = joystickTrigger(m_operatorController, kLeftTrigger);
+    private JoystickTrigger m_operatorRightTrigger = joystickTrigger(m_operatorController, kRightTrigger);
 
-  private JoystickTrigger m_operatorLeftTrigger = joystickTrigger(m_operatorController, kLeftTrigger);
-  private JoystickTrigger m_operatorRightTrigger = joystickTrigger(m_operatorController, kRightTrigger);
+    private JoystickAxes m_operatorLeftJoystick = joystickAxes(m_operatorController, kLeftX, kLeftY);
+    private JoystickAxes m_operatorRightJoystick = joystickAxes(m_operatorController, kRightX, kRightY);
+    // The robot's subsystems and commands are defined here...
 
-  private JoystickAxes m_operatorLeftJoystick = joystickAxes(m_operatorController, kLeftX, kLeftY);
-  private JoystickAxes m_operatorRightJoystick = joystickAxes(m_operatorController, kRightX, kRightY);
-  // The robot's subsystems and commands are defined here...
-  
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        // Configure the button bindings
+        configureButtonBindings();
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
 
-  }
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return new InstantCommand(Utils::noop);
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return new InstantCommand(Utils::noop);
+    }
 }
--- src/main/java/frc/robot/utils/Utils.java
+++ src/main/java/frc/robot/utils/Utils.java
@@ -1,6 +1,5 @@
 package frc.robot.utils;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
